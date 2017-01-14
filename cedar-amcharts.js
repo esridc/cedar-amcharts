64fcb987c4dd6d18251e2d187016aeef15464bec
ajturner <aturner@esri.com>
2017-01-13 19:56:44 -0500

    Added timeclock examples

--- cedar-amcharts.js
+++ cedar-amcharts.js
@@ -208,6 +208,30 @@ function getLayerQueryUrl(layer, query){
           "enabled": true
         }
       },
+      "radar": {
+        "theme": "light",
+        "type": "radar",
+        "valueAxes": [{
+          "gridType": "circles",
+          "minimum": 0
+        }],
+        "polarScatter": {
+          "minimum": 0,
+          "maximum": 400,
+          "step": 1
+        },
+        "startDuration": 0,
+        "graphs": [{}],
+        "groupPercent": 5,
+        "balloon":{
+           "fixedPosition":true
+          },
+        "legend":{
+         	"position":"right",
+          "marginRight":100,
+          "autoMargins":false
+        },
+      },
       "pie": {
         "type": "pie",
         "innerRadius": "30%",
